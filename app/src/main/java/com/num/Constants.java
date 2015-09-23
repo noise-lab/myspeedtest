8aafe8521c4aa561eb581a949bc14c36d361aa86
gmartins <ggmartins@gmail.com>
2015-09-23 18:16:18 -0400

    dns test initial commit

--- app/src/main/java/com/num/Constants.java
+++ app/src/main/java/com/num/Constants.java
@@ -32,6 +32,7 @@ public class Constants {
 
     /* Background Measurement Service */
     public static final int UPDATE_INTERVAL = 1000 * 60 * 15; // 15 minutes
+    public static final int DAILY_INTERVAL = 1000 * 60 * 60 * 24; // used only for switch id
     public static final int NORMAL_SLEEP_TIME = 1000;
     public static final int SHORT_SLEEP_TIME = 100;
     public static final int ONE_MINUTE_TIME = 60 * 1000;
--- app/src/main/java/com/num/controller/receivers/MeasurementAlarmReceiver.java
+++ app/src/main/java/com/num/controller/receivers/MeasurementAlarmReceiver.java
@@ -9,18 +9,24 @@ import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.PowerManager;
 import android.util.Log;
+import android.net.Uri;
 
 import com.num.Constants;
 import com.num.controller.managers.DataUsageManager;
+import com.num.controller.managers.DnsManager;
 import com.num.controller.managers.MeasurementManager;
 import com.num.controller.utils.DataUsageUtil;
+
+import java.util.Calendar;
+import java.util.TimeZone;
+
 
 public class MeasurementAlarmReceiver extends BroadcastReceiver {
 
     final static String TAG = "AlarmReceiver";
 
     @Override
-    public void onReceive(Context context, Intent arg1) {
+    public void onReceive(Context context, Intent intent) {
         try {
             PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
             PowerManager.WakeLock wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "");
@@ -30,29 +36,58 @@ public class MeasurementAlarmReceiver extends BroadcastReceiver {
             dataUsageManager.execute(context);
 
             // run only when background_service is set to run (true)
-            SharedPreferences prefs = context.getSharedPreferences(Constants.SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE);
-            if (prefs.getBoolean("accept_conditions", false) && prefs.getBoolean("background_service", false)) {
-                Log.d(TAG, "background service");
-                MeasurementManager manager = new MeasurementManager(context);
-                manager.execute();
-                try {
-                    Thread.sleep(Constants.SHORT_SLEEP_TIME);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+            switch(intent.getIntExtra("repeating", 0))
+            {
+                case Constants.UPDATE_INTERVAL:
+                    Log.d(TAG, "Background Interval Update");
+                    SharedPreferences prefs = context.getSharedPreferences(Constants.SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE);
+                    if (prefs.getBoolean("accept_conditions", false) && prefs.getBoolean("background_service", false)) {
+                        MeasurementManager manager = new MeasurementManager(context);
+                        manager.execute();
+                        try {
+                            Thread.sleep(Constants.SHORT_SLEEP_TIME);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+                case Constants.DAILY_INTERVAL:
+                    Log.d(TAG, "Background Daily Update");
+                    DnsManager dnsManager=new DnsManager(context);
+                    dnsManager.execute();
+                    break;
+                default:
+
             }
+
             wakeLock.release();
         }catch(Exception e){
             Log.d(TAG, "onReceive");
         }
     }
 
+
     public void setAlarm(Context context) {
         AlarmManager manager=(AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
-        Intent intent = new Intent(context, MeasurementAlarmReceiver.class);
+        Intent intent = new Intent("update", Uri.parse("interval"), context, MeasurementAlarmReceiver.class);
+        intent.putExtra("repeating", Constants.UPDATE_INTERVAL);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
         manager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), Constants.UPDATE_INTERVAL, pendingIntent);
     }
+
+
+    public void setAlarmDaily(Context context) {
+        AlarmManager manager=(AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent("update", Uri.parse("daily"), context, MeasurementAlarmReceiver.class);
+        intent.putExtra("repeating", Constants.DAILY_INTERVAL);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        Calendar updateTime = Calendar.getInstance();
+        TimeZone tz = updateTime.getTimeZone();
+        updateTime.set(Calendar.HOUR_OF_DAY, 00);
+        updateTime.set(Calendar.MINUTE, 40);
+        manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, updateTime.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+    }
+
 
     public void cancelAlarm(Context context) {
         Intent intent = new Intent(context, MeasurementAlarmReceiver.class);
