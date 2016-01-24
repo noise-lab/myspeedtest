cd5e0653e9d1e55b9d80579e657eadd4449c647a
Ben Jones <benjam2@g.clemson.edu>
2016-01-24 09:43:43 -0500

    adding interference measurement consent

---
+++ app/src/main/java/com/num/view/activities/InterferenceConsentActivity.java
@@ -0,0 +1,13 @@
+package com.num.view.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InterferenceConsentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interference_consent);
+    }
+}
